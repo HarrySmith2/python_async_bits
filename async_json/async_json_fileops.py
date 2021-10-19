5b993d6f3a9fe299275adcc27b8aaccee4ac8309
Harry Smith <33291554+HarrySmith2@users.noreply.github.com>
2021-10-19 21:22:10 +0100

    Added async Json load/write

---
+++ async_json/async_json_fileops.py
@@ -0,0 +1,49 @@
+from json import loads, dumps
+
+import asyncio
+from aiofiles import open
+
+
+__doc__ = """
+Writing to JSON via 'dump' is a syncronous (blocking) action.
+Cheap Async fix for this is to generate file pointer via
+aiofiles, append new data to json data via async_load
+then json.dumps the new data.
+"""
+
+
+async def load(filename):
+    """Async load from JSON file"""
+    async with open(filename, "rb") as f:
+        return loads(await f.read())
+
+
+async def write(filename, data, indent=0):
+    """Async write to JSON file"""
+    async with open(filename, "w+") as f:
+        await f.write(f"{dumps(data, indent=indent)}")
+
+
+async def bla():
+    """Whatever async print to test above"""
+    print("bla")
+
+
+async def main():
+    for i in range(10):
+        await bla()
+        data = await load("test.json")
+        print(data, type(data))
+
+        data[len(data.keys()) + 1] = "test"
+
+        await write("test.json", data, 4)
+
+    # With more complex structure
+    data = await load("test.json")
+    data[len(data.keys()) + 1] = [{"list_dict": "test"}]
+    await write("test.json", data, 4)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
